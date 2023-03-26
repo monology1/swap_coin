9e916b755fa0ae4b98ad1439cf0137206692e064
bob <58608445+monology1@users.noreply.github.com>
2023-03-26 20:05:16 +0700

    commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
+import { AppProps } from 'next/app'
+import { useState } from 'react'
+import Nav from './components/Nav'
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  const [darkMode, setDarkMode] = useState(false)
+
+  return (
+    <>
+      <div className={`${darkMode ? 'bg-gray-900 text-white' : 'bg-gray-100 text-black'} min-h-screen flex flex-col`} style={{ fontFamily: 'Kanit' }}>
+        <Nav darkMode={darkMode} setDarkMode={setDarkMode} />
+        <Component {...pageProps} />
+      </div>
+    </>
+  )
 }
+
+export default MyApp
